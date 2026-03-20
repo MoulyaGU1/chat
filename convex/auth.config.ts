d915622ad8d96521d1a0ccd67aa373363d84d033
MoulyaGU1 <moulyagu@gmail.com>
2026-03-20 12:59:39 +0530

    final deploy fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://square-dinosaur-43.clerk.accounts.dev",
+      domain: "square-dinosaur-43.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
