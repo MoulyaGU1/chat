b2b3bcc93a07475a3dfdee125766947adc0dc0cb
MoulyaGU1 <moulyagu@gmail.com>
2026-03-20 11:53:05 +0530

    fix convex url

--- lib/convexclient.ts
+++ lib/convexclient.ts
@@ -1,3 +1,9 @@
 import { ConvexReactClient } from "convex/react";
 
-export const convexClient = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+if (!convexUrl) {
+  throw new Error("NEXT_PUBLIC_CONVEX_URL is not set");
+}
+
+export const convex = new ConvexReactClient(convexUrl);
